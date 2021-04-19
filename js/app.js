5038641f8994bbc0d734946ecf46d5c65fae6024
Raya22 <raya.lobany94@gmail.com>
2021-04-19 23:29:40 +0300

    constructor

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-let Time = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let Time = ['  6am', '  7am', '  8am', '  9am', '  10am', '  11am', '  12pm', '  1pm', '  2pm', '  3pm', '  4pm', '  5pm', '  6pm', '  7pm'];
 
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
@@ -10,7 +10,94 @@ function getRandomNumber(min, max) {
 }
 
 
-let SeattleShop = {
+
+
+
+function Shop(location,MinCust,MaxCust,AvgCookieSale,SalesPerHour=[]){
+  this.location=location;
+  this.MinCust=MinCust;
+  this.MaxCust;
+  this.AvgCookieSale=AvgCookieSale;
+  this.SalesPerHour=SalesPerHour;
+  this.TotalSales=0;
+  this.RandomSaleAmount= 0;
+}
+
+Shop.prototype.getCustNumber=function() {
+  this.RandomSaleAmount = getRandomNumber(this.MinCust, this.MaxCust) * this.AvgCookieSale;
+  console.log(this.RandomSaleAmount);
+  return this.RandomSaleAmount;
+};
+
+
+Shop.prototype.DailySailesPerHour=function () {
+  for (let i = 0; i < Time.length; i++) {
+    let RandomSalesNumber = Math.ceil(this.getCustNumber());
+    this.SalesPerHour.push(RandomSalesNumber);
+    this.TotalSales += RandomSalesNumber;
+
+  }
+
+};
+
+
+Shop.prototype.render=function () {
+  let parentElement = document.getElementById('cookies');
+  let h2Element = document.createElement('h2');
+  h2Element.textContent = (Shop.location);
+  parentElement.appendChild(h2Element);
+
+  let table = document.createElement('table');
+  parentElement.appendChild(table);
+
+  for (let i = 0; i < Time.length; i++) {
+    let TableRow = document.createElement('th');
+    TableRow.textContent = Time[i];
+    table.appendChild(TableRow);
+  
+
+  }
+  let ceties=['   Seattel  ','Tokyo','Dubai','Paris','Lima'];
+
+  
+
+  for (let i = 0; i < ceties.length; i++) {
+    let TableRow = document.createElement('tr');
+    TableRow.textContent = ceties[i];
+    table.appendChild(TableRow);
+  }
+
+  for (let i = 0; i < Time.length; i++) {
+    let TabelBody=document.createElement('tbody');
+    TabelBody.textContent(TableRow);
+    table.appendChild(TabelBody);
+
+    let Tablecol = document.createElement('tr');
+    Tablecol.textContent = this.SalesPerHour[i] + ' cookies';
+    table.appendChild(Tablecol);
+  }
+
+
+  let Tablefooter = document.createElement('tfoot');
+  Tablefooter.textContent = 'Total ' + this.TotalSales;
+  table.appendChild(Tablefooter);
+};
+
+let raya=new Shop('oroba',3,9,2);
+raya.DailySailesPerHour();
+raya.render();
+
+
+
+
+
+
+
+
+
+
+
+/*let SeattleShop = {
   location: 'Seattle',
   MinCust: 23,
   MaxCust: 65,
@@ -265,4 +352,4 @@ let LimaShop = {
 };
 LimaShop.DailySailesPerHour();
 LimaShop.render();
-
+*/
