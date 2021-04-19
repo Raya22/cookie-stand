c8ed1da1535f2b52ae9aa5bf2007278bea1bef33
Raya22 <raya.lobany94@gmail.com>
2021-04-19 18:45:26 +0300

    add read8

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable new-cap */
 'use strict';
 
 
@@ -8,6 +9,65 @@ function getRandomNumber(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
+
+
+function Shop(location,MinCust,MaxCust,AvgCookieSale,SalesPerHour)
+{
+  this.location=location;
+  this.MinCust=MinCust;
+  this.MaxCust=MaxCust;
+  this.AvgCookieSale=AvgCookieSale;
+  this.SalesPerHour=SalesPerHour;
+  this.TotalSales=0;
+  //this.RandomSalesNumber=0;
+
+}
+
+
+
+Shop.prototype.getCustNumber= function () {
+  this.RandomSaleAmount = getRandomNumber(this.MinCust, this.MaxCust) * this.AvgCookieSale;
+  //console.log(this.RandomSaleAmount);
+  return this.RandomSaleAmount;
+};
+
+Shop.prototype.DailySailesPerHour= function () {
+  for (let i = 0; i < Time.length; i++) {
+    let RandomSalesNumber = Math.ceil(this.getCustNumber());
+    this.SalesPerHour.push(RandomSalesNumber);
+    this.TotalSales += RandomSalesNumber;
+
+  }
+};
+
+
+Shop.prototype.render= function () {
+  let parentElement = document.getElementById('cookies');
+  let h2Element = document.createElement('h2');
+  h2Element.textContent = ('Seattle');
+  parentElement.appendChild(h2Element);
+
+  let list = document.createElement('ul');
+  parentElement.appendChild(list);
+
+  for (let i = 0; i < Time.length; i++) {
+    let ListItem = document.createElement('li');
+    ListItem.textContent = Time[i] + ' : ' + this.SalesPerHour[i] + ' cookies';
+    list.appendChild(ListItem);
+  }
+
+  let ListItem = document.createElement('li');
+  ListItem.textContent = 'Total ' + this.TotalSales;
+  list.appendChild(ListItem);
+};
+
+//let SSR=new Shop();
+//SSR.DailySailesPerHour();
+//SSR.render();
+
+
+
+
 
 
 let SeattleShop = {
@@ -59,8 +119,6 @@ let SeattleShop = {
 
 SeattleShop.DailySailesPerHour();
 SeattleShop.render();
-
-
 
 
 
@@ -164,7 +222,6 @@ DubaiShop.DailySailesPerHour();
 DubaiShop.render();
 
 
-
 let ParisShop = {
   location: 'Paris',
   MinCust: 20,
@@ -214,8 +271,7 @@ let ParisShop = {
 };
 ParisShop.DailySailesPerHour();
 ParisShop.render();
-
-
+ 
 
 let LimaShop = {
   location: 'Lima',
@@ -265,4 +321,3 @@ let LimaShop = {
 };
 LimaShop.DailySailesPerHour();
 LimaShop.render();
-
